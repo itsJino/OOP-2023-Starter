1e048602e28adb2782d2b282ae9d84053f055667
Joshua <floresjoshua2603@gmail.com>
2023-04-26 16:52:11 +0100

    loadFile

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -3,6 +3,8 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import ie.tudublin.Follow;
+import ie.tudublin.Word;
 
 public class DANI extends PApplet {
 	public void settings() {
@@ -11,6 +13,8 @@ public class DANI extends PApplet {
 	}
 
     String[] sonnet;
+	String[] s;
+	String[] w;
 
     public String[] writeSonnet()
     {
@@ -36,5 +40,54 @@ public class DANI extends PApplet {
 		noStroke();
 		textSize(20);
         textAlign(CENTER, CENTER);
+
+		loadFile();
+	}
+
+	public void loadFile() {
+		s = loadStrings("small.txt");
+
+		for(int i = 0; i < s.length; i++)
+		{
+			w =  split(s[i], ' ');
+		}
+		
+		for (int i = 0; i < w.length; i++) {
+			w[i] = w[i].replaceAll("[^\\w\\s]", "");
+		}
+
+		for (int i = 0; i < w.length; i++) {	
+			w[i] = w[i].toLowerCase();
+		}	
+
+		for(int i = 0; i < w.length; i++)
+		{
+			System.out.println(w[i]);
+		}
+	}
+
+	public Boolean findWord(String search) {
+		for (int i = 0; i < w.length; i++) {
+			if (w[i].equals(search)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void findFollow(String search) {
+		Follow follow = new Follow();
+		for (int i = 0; i < w.length; i++) {
+			if (w[i].equals(search)) {
+				follow.setWord(w[i + 1]);
+				follow.setCount(follow.getCount() + 1);
+			}
+		}
+	}
+
+	public void printModel() {
+		for (int i = 0; i < w.length; i++) {
+			System.out.println(w[i]);
+		}
 	}
 }
